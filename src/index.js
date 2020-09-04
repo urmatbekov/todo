c17f24150e488041eaabdfb1e78495253765ff51
Eldiyar <eldiiardev@gmail.com>
2020-09-04 09:51:33 +0600

    react-jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Hello = () => {
+    const style = {
+        color: 'red',
+        fontSize: '15px'
+    }
+    return (
+        <div>
+            <h1 style={style} className='text-center'>React first App</h1>
+            <p>it is good news</p>
+            <div className='form-group'>
+                <label htmlFor='UserName'>Username</label>
+                <input type='text' name='username' id='UserName'/>
+            </div>
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const Blog = (props) => {
+    return (
+        <div className="card" style={{width:'18rem'}} >
+            <img className="card-img-top" src="https://www.biggerbolderbaking.com/wp-content/uploads/2017/08/1C5A0056-500x500.jpg" alt="Card image cap"/>
+            <div className="card-body">
+                <h5 className="card-title">{props.title}</h5>
+                <p className="card-text">{props.text}</p>
+            </div>
+        </div>
+    )
+}
+
+const Products = (props) => {
+    return (
+        <div style={{width:"min-content",margin:"auto",display:"flex"}}>
+            <Blog title={'Tort'} text={'diur gsiug iurguerg pieurg piuergf pwure'}/>
+            <Blog title={'Shocolad'}/>
+            <Blog title={'Marmilad'}/>
+            <Blog title={'Yogurt'}/>
+        </div>
+    )
+}
+
+
+ReactDom.render(
+    <Products/>
+    , document.getElementById('root'))
